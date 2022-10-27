3ceda01443e5499410e7028a835c9c96523ecfc8
DanielOkyere <daniel.kwame.okyere101@gmail.com>
2022-10-27 12:35:46 +0100

    On branch master  Your branch is up to date with 'origin/master'.
     Changes to be committed:
    	modified:   0-setup_web_static.sh
    	new file:   0-transfer_file
    	new file:   1-pack_web_static.py

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,8 +26,7 @@ echo -e "\e[1;32m prevent overwrite\e[0m"
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data
 
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}'
-
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 echo -e "\e[1;32m Symbolic link created\e[0m"
 
