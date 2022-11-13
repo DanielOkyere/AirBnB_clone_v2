9f05897166b5c3fc372774394e9b1614c23a1a16
Brian-Machiestay <brianmachiestay@gmail.com>
2022-11-13 03:34:54 +0000

    Revert Airbnb clone
    This reverts commit b7ea7985e6926fa0e878ec6073f996044b9e3e39.

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Script starts a Flask Web Application
+Runs on `Port: 5000, host: 0.0.0.0`
+Implements strict_slashes=False
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_route():
+    """
+    / Returns "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
